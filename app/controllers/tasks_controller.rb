b96e8a6fdeed28cf68fe7cca4878778a5806ce07
Dai Osada <dai.osada@gmail.com>
2020-06-30 13:51:44 +0000

    Finish C5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,30 @@
 class TasksController < ApplicationController
+  before_action :set_user, only: [:index, :new, :create]
+  
   def index
-    @user = User.find(params[:user_id])
     @tasks = @user.tasks
   end
   
   def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url
+    else
+      render :new
+    end
   end
   
   def show
   end
+  
+  private
+    
+    def task_params
+      params.require(:task).permit(:name, :detail)
+    end
 end
