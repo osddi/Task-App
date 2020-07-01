d81348793af849be4efa6f040828a62810e64717
Dai Osada <dai.osada@gmail.com>
2020-07-01 15:22:43 +0000

    Finish C8

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_user, only: [:index, :new, :create]
+  before_action :set_user, only: [:index, :new, :create, :edit]
+  before_action :set_task, only: [:edit, :update, :show]
   
   def index
     @tasks = @user.tasks
@@ -10,7 +11,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = Task.new(name: params[:name], detail: params[:detail], user_id: @user.id)
     if @task.save
       flash[:success] = "タスクを新規作成しました。"
       redirect_to user_tasks_url
@@ -19,10 +20,26 @@ class TasksController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @task.update_attributes(task_params)
+      flash[:success] = "タスクを更新しました。"
+      redirect_to user_task_url([@user, @task])
+    else
+      render :edit
+    end
+  end
+  
   def show
   end
   
   private
+    
+    def set_task
+      @task = Task.find(params[:id])
+    end
     
     def task_params
       params.require(:task).permit(:name, :detail)
